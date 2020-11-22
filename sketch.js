1f11e9a1066ef44f5387bedf7404235754bf18a8
Charu Singh <55341559+charu-singh-rajput@users.noreply.github.com>
2020-11-22 10:53:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,34 @@
-
-var fixedRect,movingRect;
+var fixedRect, movingRect;
 
 function setup() {
-  createCanvas(800,400);
-  fixedRect=createSprite(400,200,50,80);
-  fixedRect.shapeColor="Green";
-  fixedRect.debug=true;
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 
-  movingRect=createSprite(600,200,80,30);
-  movingRect.shapeColor="Green";
-  movingRect.debug=true;
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background("black");  
+  background(0,0,0);  
 
-    movingRect.x=World.mouseX;
-    movingRect.y=World.mouseY;
-
-    if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2)
-    {
-      movingRect.shapeColor="red";
-      fixedRect.shapeColor="red";
-    }
-    else
-    {
-      movingRect.shapeColor="green";
-      fixedRect.shapeColor="green";
-    }
-
+  bounceOff(movingRect,fixedRect);
   drawSprites();
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+    object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1);
+  }
+  if (object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY * (-1);
+  } 
 }
