a894f01cb235a9f82dc84a198b5acb759bdd85a2
Charu Singh <55341559+charu-singh-rajput@users.noreply.github.com>
2020-11-25 14:29:18 +0530

    Add files via upload

---
+++ Boxx.js
@@ -0,0 +1,23 @@
+class Boxx
+{
+    constructor(x,y,width,height)
+    {
+        var options={
+            restitution:1
+        }
+
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.body);
+    }
+    display()
+    {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+
+
+
+
+}
